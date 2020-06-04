91d8942744fd81c66acd6a5a5816de65c648868b
thiagovasconcellos <thiagovasconcellos88@gmail.com>
2020-06-04 14:36:36 -0300

    rotas controllers e services

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuth from '@modules/users/infra/http/middlewares/ensureAuth';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuth);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentController.index);
 
 export default appointmentsRouter;
