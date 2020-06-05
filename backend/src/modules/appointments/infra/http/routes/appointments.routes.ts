20ad02384d29f1610876991082a23a02d019cc59
thiagovasconcellos <thiago.vasconcellos@cbyk.com.br>
2020-06-05 15:16:49 -0300

    celebrate-validacoes de rotas

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuth from '@modules/users/infra/http/middlewares/ensureAuth';
 import AppointmentsController from '../controllers/AppointmentsController';
@@ -10,7 +11,16 @@ const providerAppointmentController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuth);
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+);
 appointmentsRouter.get('/me', providerAppointmentController.index);
 
 export default appointmentsRouter;
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -3,6 +3,7 @@ import '@shared/container';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
@@ -21,6 +22,7 @@ app.use(
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
