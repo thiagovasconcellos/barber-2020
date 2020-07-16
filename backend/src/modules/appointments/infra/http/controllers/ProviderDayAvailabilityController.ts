c797e5d25218fe23415d4ec5464e24b66f4a68c4
thiagovasconcellos <thiagovasconcellos88@gmail.com>
2020-07-16 14:44:36 -0300

    corrigindo rotas

--- backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -6,15 +6,15 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year, day } = request.body;
+    const { month, year, day } = request.query;
 
     const listProviders = container.resolve(ListProviderDayAvailabilityService);
 
     const availability = await listProviders.execute({
       provider_id,
-      month,
-      year,
-      day,
+      month: Number(month),
+      year: Number(year),
+      day: Number(day),
     });
 
     return response.json(availability);
